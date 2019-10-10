9649c2f68a41b2df04bfaf88b3b3adf4023f5164
Ray <lyraymond53@gmail.com>
2019-10-09 17:56:41 -0700

    Reclone from classroom acc

---
+++ cmake-build-debug/src/CMakeFiles/meshedit.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/bezierCurve.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/bezierCurve.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/bezierPatch.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/bezierPatch.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/camera.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/camera.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/collada.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/collada.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/halfEdgeMesh.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/halfEdgeMesh.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/light.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/light.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/main.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/main.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/material.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/material.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/mesh.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/mesh.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/meshEdit.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/meshEdit.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/png.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/png.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/scene.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/scene.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/student_code.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/student_code.cpp.o"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/src/texture.cpp" "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/src/CMakeFiles/meshedit.dir/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "/Library/Frameworks/Mono.framework/Headers"
+  "/usr/local/include/freetype2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/Users/raymond/Documents/CS184/p2-meshedit-lyraymond53/cmake-build-debug/CGL/deps/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
